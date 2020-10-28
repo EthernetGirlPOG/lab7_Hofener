1bfe2bd0790e956481641c1b4893b7ffd6526e3c
Hofener <vhofene@rockets.utoledo.edu>
2020-10-28 11:14:05 -0400

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package HelloWorld;
+
+class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world, this is Victoria!");
+	}
+
+}
+# lab7_Hofener
